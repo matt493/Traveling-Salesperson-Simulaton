051454b858bdac4a240ff673990a6b8fd2dd36c4
mathewreji493 <mathewreji493@gmail.com>
2021-05-12 10:21:20 +0530

    progress bar and optimisations

--- sketch.js
+++ sketch.js
@@ -5,8 +5,10 @@ let button, canvas;
 let running = false;
 
 let townStack = [];
-let bestTour = [];
-let bestDistance = 0;
+let bestTour;
+let bestDistance;
+let totalSolutions;
+let solutionsExplored;
 
 class Town
 {
@@ -20,6 +22,8 @@ class Town
 }
 
 //#region Util Funcs
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 function drawNode(node)
 {
 	fill('white');
@@ -33,7 +37,7 @@ function drawEdge(start, end)
 	let x0 = start.x,	y0 = start.y;
 	let x1 = end.x, 	y1 = end.y;
 
-	stroke(120);
+	stroke(110);
 	strokeWeight(4);
 	line(x0, y0, x1, y1);
 }
@@ -46,11 +50,44 @@ function distanceBetween(start, end)
 	return Math.sqrt(Math.pow(x1-x0,2) +  Math.pow(y1-y0,2));
 }
 
+async function run()
+{
+	running = true;
+	clear();
+	setup();
+	loop();
+	totalSolutions = fact(townStack.length) / fact(townStack.length - townStack.length);
+
+	if(townStack.length == 1){}	//only 1 town, do nothing
+	else if(townStack.length == 2)	//2 towns, best path is straight line between them
+	{
+		bestTour = [...townStack];
+	}
+	else
+	{
+		await generatePermutations(townStack, townStack.length);	//generates permutations and stores in solutions[]
+	}
+	// drawUI();
+	noLoop();
+	bestDistance = 0;
+	for (let i = 0; i < bestTour.length; i++)
+	{
+		let nxtTown = (i+1) % bestTour.length;
+		bestDistance += distanceBetween(bestTour[i], bestTour[nxtTown]);
+
+		drawEdge(bestTour[i], bestTour[nxtTown]);
+	}
+	print('Best Distance:', bestDistance);
+
+	running = false;
+}
+
 async function generatePermutations(arr, size)	//Heap's Algorithm to generate permutations
 {
 	if(size == 1)
 	{
-		await evaluateSolution(arr);
+		// print(arr);
+		await evaluateSolution([...arr]);
 		return;		// if size becomes 1 then print the obtained permutation
 	}
 	else
@@ -69,38 +106,71 @@ async function generatePermutations(arr, size)	//Heap's Algorithm to generate pe
 
 async function evaluateSolution(solution)
 {
-	if(solution.length <= 2) {}
-	else if(solution[0].index < solution[solution.length - 2].index)	//don't check duplicates which are just reverse of a solution
+	// drawUI();
+	if(solution[0].index < solution[solution.length - 2].index)	//don't check duplicates which are just reverse of a solution
 	{
 		let distance = 0;
+		
 		for (let i = 0; i < solution.length; i++)
 		{
 			let nxtTown = (i+1) % solution.length;
 			distance += distanceBetween(solution[i], solution[nxtTown]);
-	
 			drawEdge(solution[i], solution[nxtTown]);
-			await delay(0);
 		}
-		if(bestDistance == 0){bestDistance = distance;}
-		else if(distance <= bestDistance)
+		if(distance < bestDistance || bestDistance == 0)
 		{
-			bestDistance = distance;
+			// drawUI();
+			bestDistance = 0;
 			bestTour = [...solution];
-			print('Best Distance Yet:', bestDistance);
+
+			for (let i = 0; i < bestTour.length; i++)
+			{
+				let nxtTown = (i+1) % bestTour.length;
+				bestDistance += distanceBetween(bestTour[i], bestTour[nxtTown]);
+				// drawEdge(bestTour[i], bestTour[nxtTown]);
+			}
 		}
-		clear();
-		drawUI();
+		await delay(0);
 	}
+	
+	drawUI();
+
+	solutionsExplored++;
+	rectMode(CORNER);
+	let fillpercentage = (solutionsExplored/totalSolutions)*200;
+	fill(100,200,100);
+	strokeWeight(0);
+	rect(100, 46, fillpercentage, 15);
 }
 
 function drawUI()
 {
 	background(50);
 	rectMode(CORNERS);
-	fill(0, 0, 0, 0);
+	fill(0, 0);
 	stroke('white');
 	strokeWeight(1);
 	rect(400, 50, windowWidth - 40, windowHeight - 50);
+
+	rectMode(CORNER);
+
+	textSize(20);
+	strokeWeight(0);
+	fill(200);
+	text('Progress:', 10, 60);
+
+	fill(0,0);
+	stroke('white');
+	strokeWeight(.5);
+	rect(100, 46, 200, 15);
+}
+
+function fact(n)
+{
+	let f = 1;
+	for(let i = 2; i <= n; i++)
+		f = f * i;
+	return f;
 }
 
 //#endregion 
@@ -111,39 +181,23 @@ function setup()
 	canvas.position(0,0);
 	
 	button = createButton('RUN');
-	button.position(10, 10);
+	button.position(10, 200);
 	button.mouseClicked(run);
 
-	drawUI();
-}
-
-async function run()
-{
-	running = true;
-	clear();
-	setup();
-	
-	await generatePermutations(townStack, townStack.length);	//generates permutations and stores in solutions[]
-
-	let distance = 0;
-	for (let i = 0; i < bestTour.length; i++)
-	{
-		let nxtTown = (i+1) % bestTour.length;
-		distance += distanceBetween(bestTour[i], bestTour[nxtTown]);
-
-		drawEdge(bestTour[i], bestTour[nxtTown]);
-	}
-	print('Best Distance:', distance);
+	bestTour = [];
 	bestDistance = 0;
-	running = false;
+
+	solutionsExplored = 0;
+	totalSolutions = 0;
+
+	drawUI();
+	noLoop();
 }
 
 function draw()
 {
 	townStack.forEach(element => { drawNode(element); });
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function mousePressed()
 {
